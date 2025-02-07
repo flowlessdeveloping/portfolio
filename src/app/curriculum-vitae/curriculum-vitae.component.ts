b88414a667abd20d3fa0c7f759d508f7e9c4bacd
davide <davide.valenti393@gmail.com>
2025-02-07 15:13:47 +0100

    first layout

--- src/app/curriculum-vitae/curriculum-vitae.component.ts
+++ src/app/curriculum-vitae/curriculum-vitae.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatExpansionModule } from '@angular/material/expansion'; 
+import { MatListModule } from '@angular/material/list';
+import { CvService } from '../service/cvData.service';
+import {CvModel} from '../model/cv.model';
 
 @Component({
   selector: 'app-curriculum-vitae',
-  imports: [MatCardModule],
+  imports: [
+    MatCardModule,
+    MatExpansionModule,
+    MatListModule,
+    CommonModule
+  ],
   templateUrl: './curriculum-vitae.component.html',
   styleUrl: './curriculum-vitae.component.css'
 })
-export class CurriculumVitaeComponent {
+
+
+export class CurriculumVitaeComponent implements OnInit {
+
+  cvData: CvModel[] = [];
+
+  constructor(private cvService: CvService) {}
+
+  ngOnInit(): void {
+    this.cvService.getCvData().subscribe(data => {
+      this.cvData = data;
+    });
+  }
+ 
 
 }
---
+++ src/app/model/cv.model.ts
@@ -0,0 +1,11 @@
+export interface CvModel {
+    section: string;
+    details: {
+      title: string;
+      description: string;
+      technologies: string;
+    }[];
+  }
+  
+
+  
