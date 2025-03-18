519b8c398889eedb463705d5b169ba575ff036b9
davide <davide.valenti393@gmail.com>
2025-03-18 10:41:26 +0100

    situazione stabile, pagina principale con funzionalità di showing e mobile adaptation

--- src/app/curriculum-vitae/curriculum-vitae.component.ts
+++ src/app/curriculum-vitae/curriculum-vitae.component.ts
@@ -6,14 +6,24 @@ import { MatIconModule } from '@angular/material/icon';
 import { CvModel } from '../model/cv.model';
 import { CvService } from '../service/cvData.service';
 import { IntroductionModel } from '../model/introduction.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-curriculum-vitae',
-  imports: [MatCardModule, MatDividerModule, MatListModule, MatIconModule],
+  imports: [
+    MatCardModule,
+    MatDividerModule,
+    MatListModule,
+    MatIconModule,
+    CommonModule,
+  ],
   templateUrl: './curriculum-vitae.component.html',
   styleUrl: './curriculum-vitae.component.css',
 })
 export class CurriculumVitaeComponent implements OnInit {
+  showDetailedCard = false;
+  currentview = '';
+  animateCard = false;
   cvData: CvModel[] = [];
   introduction: IntroductionModel = {
     title: '',
@@ -23,12 +33,25 @@ export class CurriculumVitaeComponent implements OnInit {
   constructor(private cvService: CvService) {}
 
   ngOnInit(): void {
-    this.cvService.getCvData().subscribe((data) => {
-      this.cvData = data;
-    });
+    // this.cvService.getCvData().subscribe((data) => {
+    //   this.cvData = data;
+    // });
 
     this.cvService.getIntroduction().subscribe((data) => {
       this.introduction = data;
     });
+  }
+
+  activeDetailedCard(chosenView: string) {
+    if (chosenView !== this.currentview && this.showDetailedCard) {
+      this.currentview = chosenView;
+      return;
+    } else {
+      this.currentview = chosenView;
+      setTimeout(() => {
+        this.animateCard = !this.animateCard;
+      }, 10);
+      this.showDetailedCard = !this.showDetailedCard;
+    }
   }
 }
---
+++ src/app/cv-details/cv-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cv-details',
+  imports: [],
+  templateUrl: './cv-details.component.html',
+  styleUrl: './cv-details.component.css'
+})
+export class CvDetailsComponent {
+
+}
